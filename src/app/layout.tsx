732b5555dd06683d07effb1f0bbb150d02caaa66
Gaurav Singh <gksingh947@gmail.com>
2024-12-07 18:27:29 +0530

    Middleware Fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Playfair_Display, Inter } from "next/font/google";
-import { ClerkProvider } from "@clerk/nextjs";
+import { ClerkProvider} from '@clerk/nextjs'
+ 
 
 const playfair = Playfair_Display({ subsets: ["latin"] });
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
@@ -10,14 +11,17 @@ export const metadata: Metadata = {
   title: "Maison - Luxury Ai Designer",
   description:
     "Transform your space with AI-powered luxury interior design solutions",
+    icons:{
+      icon: './favicon.ico',
+    }
 };
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <ClerkProvider>
+  return ( 
+     <ClerkProvider>  
     <html lang="en">
       <body className={`${inter.variable} ${playfair.className}`}>
         {children}
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,10 +3,10 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isProtectedRoute = createRouteMatcher(['/dashboard(.*)', '/forum(.*)']);
 
 export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) {
-    await auth.protect();  
-  }
-});
+
+  if (isProtectedRoute(req)) await auth.protect();  // Error occurs here
+  
+  }); 
 
 export const config = {
   matcher: [
