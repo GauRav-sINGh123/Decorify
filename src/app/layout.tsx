055a347592e8f72134b820e2f477c2eb8d06969c
Gaurav Singh <gksingh947@gmail.com>
2024-12-07 18:32:21 +0530

    Suspense Added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Playfair_Display, Inter } from "next/font/google";
 import { ClerkProvider} from '@clerk/nextjs'
+import {Suspense} from "react"
  
 
 const playfair = Playfair_Display({ subsets: ["latin"] });
@@ -21,12 +22,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return ( 
-     <ClerkProvider>  
+    <Suspense fallback={<div>Loading...</div>}>
+     <ClerkProvider dynamic>  
     <html lang="en">
       <body className={`${inter.variable} ${playfair.className}`}>
         {children}
       </body>
     </html>
     </ClerkProvider>
+    </Suspense>
   );
 }
