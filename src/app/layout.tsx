d6f5c3e71fc2e8991a9397cf45b156c7612c4b54
Gaurav Singh <gksingh947@gmail.com>
2024-12-09 20:35:29 +0530

    Clerk Error Fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,32 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Playfair_Display, Inter } from "next/font/google";
-import { ClerkProvider} from '@clerk/nextjs'
-import {Suspense} from "react"
+import { ClerkProvider } from "@clerk/nextjs";
  
 
 const playfair = Playfair_Display({ subsets: ["latin"] });
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export const metadata: Metadata = {
-  title: "Maison - Luxury Ai Designer",
+  title: "Maison - Luxury AI Designer",
   description:
     "Transform your space with AI-powered luxury interior design solutions",
-    icons:{
-      icon: './favicon.ico',
-    }
+  icons: {
+    icon: "./favicon.ico",
+  },
 };
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return ( 
-    <Suspense fallback={<div>Loading...</div>}>
-     <ClerkProvider dynamic>  
-    <html lang="en">
-      <body className={`${inter.variable} ${playfair.className}`}>
-        {children}
-      </body>
-    </html>
+  return (
+    <ClerkProvider >
+        <html lang="en">
+          <body className={`${inter.variable} ${playfair.className}`}>
+            {children}
+          </body>
+        </html>
     </ClerkProvider>
-    </Suspense>
   );
 }
