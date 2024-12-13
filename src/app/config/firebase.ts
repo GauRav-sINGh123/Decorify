905365f42564f9316ad68a20e0e88e0b85a62dd0
Gaurav Singh <gksingh947@gmail.com>
2024-12-14 00:03:29 +0530

    Firebase Storage Setup

--- src/app/config/firebase.ts
+++ src/app/config/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import {getStorage} from "firebase/storage";
 
  
 const firebaseConfig = {
@@ -14,5 +15,5 @@ const firebaseConfig = {
  
 const app = initializeApp(firebaseConfig);
 
- 
+export const storage=getStorage(app);
 export const db = getFirestore(app);
