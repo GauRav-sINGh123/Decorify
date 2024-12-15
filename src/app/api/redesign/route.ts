c3a5c5a321f2a317f2590c2f5565168efa8d024f
Gaurav Singh <gksingh947@gmail.com>
2024-12-15 22:35:23 +0530

    Api Env Error Fix

--- src/app/api/redesign/route.ts
+++ src/app/api/redesign/route.ts
@@ -5,7 +5,7 @@ import { db } from "@/app/config/firebase";
 import Replicate from "replicate";
 
 const replicate = new Replicate({
-  auth: process.env.REPLICATE_API_TOKEN!,
+  auth: process.env.NEXT_PUBLIC_REPLICATE_API_TOKEN,
 });
 
 export async function POST(req: NextRequest) {
